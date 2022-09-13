1b14a3374a511d966657f13f8ce67d60ee0b4cd4
shahram-creater <109846184+shahram-creater@users.noreply.github.com>
2022-09-13 05:02:34 -0700

    Add blackjack file via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el") 
+
+function getRandomCard() {
+    let randomNumer = Math.floor( Math.random()*13 ) + 1
+
+    if (randomNumer > 10) {
+        return 10
+    } else if (randomNumer === 1) {
+        return 11
+    } else {
+        return randomNumer
+    }
+}
+
+
+
+
+function startGame() {
+   
+    cardsEl.innerText = "cards:" 
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+
+    sumEl.innerText = "sum: " + sum
+    
+
+
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newcard(){
+
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        startGame()        
+    }
+}
